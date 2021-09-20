241bec956464db3847246f2b45dd594b7275382c
Sofie Nordin <S.Nordin@ucas.ac.uk>
2021-09-20 13:39:07 +0100

    new

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Net;
 
 namespace BusBoard.ConsoleApp
 {
@@ -10,6 +13,22 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
-    }
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var client = new RestClient("https://api.tfl.gov.uk/");
+            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+            var request = new RestRequest("StopPoint/490008660N/Arrivals", DataFormat.Json);
+
+            var response = client.Get(request);
+
+            Console.WriteLine(response);
+
+            Console.Read();
+
+
+
+
+        }
   }
 }
