47fbfc6308965ef039b71f3450ccb6387b3cb90b
Claire Davies <C.Davies@ucas.ac.uk>
2021-09-21 12:02:25 +0100

    limiting output to 5

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -17,23 +17,27 @@ namespace BusBoard.ConsoleApp
     {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             
+
+            //API call to Tfl
             var client = new RestClient("https://api.tfl.gov.uk/");
+
             Console.Write("insert stopcode ");
             string stopcode = Console.ReadLine();
 
             var request = new RestRequest($"StopPoint/{stopcode}/Arrivals", Method.GET);
             request.AddHeader("app_key", "41c57b07716140dc953617f2dab194a7");
             var response = client.Execute<List<Arrival>>(request).Data;
-            
+            var SortedList = response.OrderBy(x => x.timeToStation).ToList();
+            var limitedSortedList = SortedList.GetRange(0, 5);
             //foreach(seconds in timeToStation)
 
-                foreach (var bus in response)
+                foreach (var bus in limitedSortedList)
             {
                     if (bus.timeToStation >= 0)
                     {
                         TimeSpan t = TimeSpan.FromSeconds(bus.timeToStation);
                         string answer = t.ToString(@"hh\:mm\:ss");
-                    //var list = answer.OrderByDescending(x => x).ToList();
+                    
 
                     Console.WriteLine($"Bus destination: {bus.destinationName}, bus number: {bus.lineId}, time to arrival: {answer}");
                     }
