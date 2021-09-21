1717148f30e1641afd065406d2061f6467a8bc72
Claire Davies <C.Davies@ucas.ac.uk>
2021-09-21 14:43:23 +0100

    retireving lat, lon data from postcode API

---
+++ BusBoard.ConsoleApp/Postcode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusBoard.ConsoleApp
+{
+    class Postcode
+    {
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using RestSharp;
 using RestSharp.Authenticators;
 using System.Net;
+using MarkEmbling.PostcodesIO;
 
 namespace BusBoard.ConsoleApp
 {
@@ -16,34 +17,45 @@ namespace BusBoard.ConsoleApp
         static void Main(string[] args)
     {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            Console.Write("insert postcode ");
+            string postcode = Console.ReadLine();
+            string cleanPostcode = postcode.Replace(" ", "");
+            Console.WriteLine(cleanPostcode);
+
+            //API call to postcodes.io
+
+            var baseURL = new PostcodesIOClient();
+            var postcodeRequest = baseURL.Lookup(cleanPostcode);
             
+            Console.WriteLine($"lat: {postcodeRequest.Latitude}, lon: {postcodeRequest.Longitude}");
 
             //API call to Tfl
-            var client = new RestClient("https://api.tfl.gov.uk/");
+            //var client = new RestClient("https://api.tfl.gov.uk/");
 
-            Console.Write("insert stopcode ");
-            string stopcode = Console.ReadLine();
 
-            var request = new RestRequest($"StopPoint/{stopcode}/Arrivals", Method.GET);
-            request.AddHeader("app_key", "41c57b07716140dc953617f2dab194a7");
-            var response = client.Execute<List<Arrival>>(request).Data;
-            var SortedList = response.OrderBy(x => x.timeToStation).ToList();
-            var limitedSortedList = SortedList.GetRange(0, 5);
-            //foreach(seconds in timeToStation)
 
-                foreach (var bus in limitedSortedList)
-            {
-                    if (bus.timeToStation >= 0)
-                    {
-                        TimeSpan t = TimeSpan.FromSeconds(bus.timeToStation);
-                        string answer = t.ToString(@"hh\:mm\:ss");
+
+            //var request = new RestRequest($"StopPoint/{stopcode}/Arrivals", Method.GET);
+            //request.AddHeader("app_key", "41c57b07716140dc953617f2dab194a7");
+            //var response = client.Execute<List<Arrival>>(request).Data;
+            //var SortedList = response.OrderBy(x => x.timeToStation).ToList();
+            //var limitedSortedList = SortedList.GetRange(0, 5);
+            
+
+            //    foreach (var bus in limitedSortedList)
+            //{
+            //        if (bus.timeToStation >= 0)
+            //        {
+            //            TimeSpan t = TimeSpan.FromSeconds(bus.timeToStation);
+            //            string answer = t.ToString(@"hh\:mm\:ss");
                     
 
-                    Console.WriteLine($"Bus destination: {bus.destinationName}, bus number: {bus.lineId}, time to arrival: {answer}");
-                    }
+            //        Console.WriteLine($"Bus destination: {bus.destinationName}, bus number: {bus.lineId}, time to arrival: {answer}");
+            //        }
                
                 
-            }
+            //}
             
                     
 
