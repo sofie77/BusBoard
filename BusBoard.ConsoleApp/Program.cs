30db779a3ab4d1a99a6c1ad4567ef3a5c6581d35
Claire Davies <C.Davies@ucas.ac.uk>
2021-09-21 11:18:19 +0100

    console input for stoppoint

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -16,11 +16,12 @@ namespace BusBoard.ConsoleApp
         static void Main(string[] args)
     {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-
-            var client = new RestClient("https://api.tfl.gov.uk/");
             
+            var client = new RestClient("https://api.tfl.gov.uk/");
+            Console.Write("insert stopcode ");
+            string stopcode = Console.ReadLine();
 
-            var request = new RestRequest("StopPoint/490008660N/Arrivals", Method.GET);
+            var request = new RestRequest($"StopPoint/{stopcode}/Arrivals", Method.GET);
             request.AddHeader("app_key", "41c57b07716140dc953617f2dab194a7");
             var response = client.Execute<List<Arrival>>(request).Data;
             
@@ -31,7 +32,8 @@ namespace BusBoard.ConsoleApp
                     if (bus.timeToStation >= 0)
                     {
                         TimeSpan t = TimeSpan.FromSeconds(bus.timeToStation);
-                        string answer.Sort = t.ToString(@"hh\:mm\:s");
+                        string answer = t.ToString(@"hh\:mm\:ss");
+                    //var list = answer.OrderByDescending(x => x).ToList();
 
                     Console.WriteLine($"Bus destination: {bus.destinationName}, bus number: {bus.lineId}, time to arrival: {answer}");
                     }
