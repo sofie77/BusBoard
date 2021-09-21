e94311f5a93a0a208de587b6355d596dbdce9d4a
Claire Davies <C.Davies@ucas.ac.uk>
2021-09-21 16:11:11 +0100

    creating classes to enable bridge between postcode api call and tfl arrivals api call

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -31,17 +31,32 @@ namespace BusBoard.ConsoleApp
             Console.WriteLine($"lat: {postcodeRequest.Latitude}, lon: {postcodeRequest.Longitude}");
 
             //API call to Tfl
-            //var client = new RestClient("https://api.tfl.gov.uk/");
+            var client = new RestClient("https://api.tfl.gov.uk/");
+            var APIKEY = "41c57b07716140dc953617f2dab194a7";
+
+
+            //API call using lat lon
+
+            var StopPointRequest = new RestRequest($"StopPoint/?lat={postcodeRequest.Latitude}&lon={postcodeRequest.Longitude}&stopTypes=NaptanPublicBusCoachTram&radius=500", Method.GET);
+            StopPointRequest.AddHeader("app_key", APIKEY);
+            var StopPointResponse = client.Execute<StopPointResults>(StopPointRequest).Data;
+            var closestStops = StopPointResponse.stopPoints.GetRange(0, 2);            
+
+
+            foreach(var stop in closestStops)
+            {
+                Console.WriteLine(stop.NaptanId);
+            }
 
 
 
 
             //var request = new RestRequest($"StopPoint/{stopcode}/Arrivals", Method.GET);
-            //request.AddHeader("app_key", "41c57b07716140dc953617f2dab194a7");
+            //request.AddHeader("app_key", APIKEY);
             //var response = client.Execute<List<Arrival>>(request).Data;
             //var SortedList = response.OrderBy(x => x.timeToStation).ToList();
             //var limitedSortedList = SortedList.GetRange(0, 5);
-            
+
 
             //    foreach (var bus in limitedSortedList)
             //{
@@ -49,18 +64,18 @@ namespace BusBoard.ConsoleApp
             //        {
             //            TimeSpan t = TimeSpan.FromSeconds(bus.timeToStation);
             //            string answer = t.ToString(@"hh\:mm\:ss");
-                    
+
 
             //        Console.WriteLine($"Bus destination: {bus.destinationName}, bus number: {bus.lineId}, time to arrival: {answer}");
             //        }
-               
-                
-            //}
-            
-                    
 
-                
-            
+
+            //}
+
+
+
+
+
             Console.Read();
 
 
---
+++ BusBoard.ConsoleApp/StopPointResults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusBoard.ConsoleApp
+{
+    class StopPointResults
+    {
+        public List<StopPoint> stopPoints { get; set; }
+
+    }
+}
