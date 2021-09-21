42f7d13e660371a0a0482ebedb04f708b026b18e
Claire Davies <C.Davies@ucas.ac.uk>
2021-09-21 10:08:22 +0100

    converting seconds to minutes and seconds

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -11,25 +11,32 @@ namespace BusBoard.ConsoleApp
 {
   class Program
   {
-    static void Main(string[] args)
+
+       
+        static void Main(string[] args)
     {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
             var client = new RestClient("https://api.tfl.gov.uk/");
-            //client.Authenticator = new HttpBasicAuthenticator("username", "password");
+            
 
             var request = new RestRequest("StopPoint/490008660N/Arrivals", Method.GET);
             request.AddHeader("app_key", "41c57b07716140dc953617f2dab194a7");
             var response = client.Execute<List<Arrival>>(request).Data;
-            //foreach (var minutesToStation in timeToStation)
-            //{
-
-            //}
-
+            
+            //foreach(seconds in timeToStation)
 
                 foreach (var bus in response)
             {
-                Console.WriteLine($"Bus destination: {bus.destinationName}, bus number: {bus.lineId}, seconds to arrival: {bus.timeToStation}");
+                    if (bus.timeToStation >= 0)
+                    {
+                        TimeSpan t = TimeSpan.FromSeconds(bus.timeToStation);
+                        string answer.Sort = t.ToString(@"hh\:mm\:s");
+
+                    Console.WriteLine($"Bus destination: {bus.destinationName}, bus number: {bus.lineId}, time to arrival: {answer}");
+                    }
+               
+                
             }
             
                     
@@ -47,5 +54,6 @@ namespace BusBoard.ConsoleApp
 
 
         }
+       
   }
 }
